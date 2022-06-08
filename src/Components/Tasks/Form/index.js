994bea049e00e4c7fe3463187386c676c88354a4
Paula Rinaldi <paurinaldi95@gmail.com>
2022-06-08 00:39:48 -0300

    TG-64: Fixed select input so it correctly shows actual project when clicked on edit, and placeholder when adding a new one. Tried to tidy up code better on form.

--- src/Components/Tasks/Form/index.js
+++ src/Components/Tasks/Form/index.js
@@ -1,14 +1,37 @@
 import styles from './form.module.css';
 import { useEffect, useState } from 'react';
-import FeedbackModal from '../FeedbackModal';
 import { useParams } from 'react-router-dom';
 import Button from '../../Shared/Button';
 import Input from '../../Shared/Input/InputText';
 import Select from '../../Shared/Input/InputSelect';
+import FeedbackMessage from '../../Shared/FeedbackMessage';
+import Modal from '../../Shared/Modal';
 
 const Form = () => {
   const URL = process.env.REACT_APP_API_URL;
   const [projects, setProjects] = useState([]);
+  const [projectValue, setProjectValue] = useState('');
+  const [weekValue, setWeekValue] = useState('');
+  const [dayValue, setDayValue] = useState('');
+  const [descriptionValue, setDescriptionValue] = useState('');
+  const [infoForFeedback, setInfoForFeedback] = useState({});
+  const [showFeedbackMessage, setShowFeedbackMessage] = useState(false);
+  const onChangeProject = (event) => {
+    setProjectValue(event.target.value);
+  };
+  const onChangeWeek = (event) => {
+    setWeekValue(event.target.value);
+  };
+  const onChangeDay = (event) => {
+    setDayValue(event.target.value);
+  };
+  const onChangeDescription = (event) => {
+    setDescriptionValue(event.target.value);
+  };
+  const [hoursValue, setHoursValue] = useState('');
+  const onChangeHours = (event) => {
+    setHoursValue(event.target.value);
+  };
   useEffect(() => {
     fetch(`${URL}/projects`)
       .then((res) => res.json())
@@ -16,37 +39,13 @@ const Form = () => {
         setProjects(data.data);
       });
   }, []);
-  const [projectValue, setProjectValue] = useState('');
-  const onChangeProject = (event) => {
-    setProjectValue(event.target.value);
-    console.log(event.target.value);
-  };
-  const [weekValue, setWeekValue] = useState('');
-  const onChangeWeek = (event) => {
-    setWeekValue(event.target.value);
-    console.log(event.target.value);
-  };
-  const [dayValue, setDayValue] = useState('');
-  const onChangeDay = (event) => {
-    setDayValue(event.target.value);
-    console.log(event.target.value);
-  };
-  const [descriptionValue, setDescriptionValue] = useState('');
-  const onChangeDescription = (event) => {
-    setDescriptionValue(event.target.value);
-    console.log(event.target.value);
-  };
-  const [hoursValue, setHoursValue] = useState('');
-  const onChangeHours = (event) => {
-    setHoursValue(event.target.value);
-    console.log(event.target.value);
-  };
-  const [contentFeedbackModal, setContentFeedbackModal] = useState({});
-  const [showFeedBackModal, setShowFeedbackModal] = useState(false);
-
+  const arrayToMapProjects = projects.map((project) => {
+    return {
+      id: project._id,
+      optionContent: project.name
+    };
+  });
   const taskId = useParams();
-  console.log(taskId);
-
   const title = taskId.id ? 'Update Task' : 'Add Task';
   const options = {
     method: taskId.id ? 'PUT' : 'POST',
@@ -67,7 +66,7 @@ const Form = () => {
       fetch(`${URL}/tasks/${taskId.id}`)
         .then((res) => res.json())
         .then((data) => {
-          setProjectValue(data.data.nameProject.name);
+          setProjectValue(data.data.nameProject._id);
           setWeekValue(data.data.week);
           setDayValue(data.data.day);
           setDescriptionValue(data.data.description);
@@ -82,22 +81,16 @@ const Form = () => {
       const res = await fetch(options.url, options);
       const data = await res.json();
       if (res.status == 201 || res.status == 200) {
-        setContentFeedbackModal({ title: 'Request done!', description: data.message });
-        setShowFeedbackModal(true);
+        setInfoForFeedback({ title: 'Request done!', description: data.message });
+        setShowFeedbackMessage(true);
       } else {
-        setContentFeedbackModal({ title: 'Something went wrong', description: data.message });
-        setShowFeedbackModal(true);
+        setInfoForFeedback({ title: 'Something went wrong', description: data.message });
+        setShowFeedbackMessage(true);
       }
     } catch (err) {
       console.log(err);
     }
   };
-  const arrayToMapProjects = projects.map((project) => {
-    return {
-      id: project._id,
-      optionContent: project.name
-    };
-  });
   return (
     <div className={styles.container}>
       <form onSubmit={onSubmit}>
@@ -106,7 +99,7 @@ const Form = () => {
           label="Project"
           id="project"
           name="project"
-          value={projectValue._id}
+          value={projectValue}
           onChange={onChangeProject}
           required
           placeholder="Select Project"
@@ -156,14 +149,14 @@ const Form = () => {
           <Button type="submit" className={styles.submitButton} label="Submit" />
         </div>
       </form>
-      {showFeedBackModal && (
-        <FeedbackModal
-          feedbackTitle={contentFeedbackModal.title}
-          messageContent={contentFeedbackModal.description}
-          setShowFeedbackModal={setShowFeedbackModal}
-          showFeedbackModal={showFeedBackModal}
-        />
-      )}
+      <Modal
+        isOpen={showFeedbackMessage}
+        handleClose={() => {
+          setShowFeedbackMessage(false);
+        }}
+      >
+        <FeedbackMessage infoForFeedback={infoForFeedback} />
+      </Modal>
     </div>
   );
 };
