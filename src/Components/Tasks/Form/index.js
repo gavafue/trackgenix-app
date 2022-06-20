a87794429ad46dfa8b1b516471bc6ab788d56bcf
OctavioRodriguezPaseyro <101280747+OctavioRodriguezPaseyro@users.noreply.github.com>
2022-06-20 20:42:27 -0300

    TG-94: applied react hook form to tasks screen

--- src/Components/Tasks/Form/index.js
+++ src/Components/Tasks/Form/index.js
@@ -1,5 +1,7 @@
 import styles from './form.module.css';
 import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
 import SharedForm from '../../Shared/Form';
 import Input from '../../Shared/Input/InputText';
 import Select from '../../Shared/Input/InputSelect';
@@ -9,36 +11,42 @@ import Preloader from '../../Shared/Preloader';
 import { useSelector, useDispatch } from 'react-redux';
 import { editTask, postTask } from '../../../redux/tasks/thunks';
 import { showFeedbackMessage } from '../../../redux/tasks/actions';
+import tasksValidation from 'validations/tasks';
 
 const Form = () => {
   const dispatch = useDispatch();
   const [projects, setProjects] = useState([]);
-  const [projectValue, setProjectValue] = useState('');
-  const [weekValue, setWeekValue] = useState('');
-  const [dayValue, setDayValue] = useState('');
-  const [descriptionValue, setDescriptionValue] = useState('');
-  const [hoursValue, setHoursValue] = useState('');
-
   const isPending = useSelector((state) => state.tasks.isPending);
   const showFeedback = useSelector((state) => state.tasks.showFeedbackMessage);
   const infoForFeedback = useSelector((state) => state.tasks.infoForFeedback);
   const selectedItem = useSelector((state) => state.tasks.selectedItem);
+  const isItemSelected = Boolean(Object.keys(selectedItem).length);
+  const title = isItemSelected ? 'Update Task' : 'Add Task';
   const URL = process.env.REACT_APP_API_URL;
-  const isItemSelected = Object.keys(selectedItem).length;
-  const onChangeProject = (event) => {
-    setProjectValue(event.target.value);
-  };
-  const onChangeWeek = (event) => {
-    setWeekValue(event.target.value);
-  };
-  const onChangeDay = (event) => {
-    setDayValue(event.target.value);
-  };
-  const onChangeDescription = (event) => {
-    setDescriptionValue(event.target.value);
-  };
-  const onChangeHours = (event) => {
-    setHoursValue(event.target.value);
+
+  const arrayToMapProjects = projects.map((project) => {
+    return {
+      id: project._id,
+      optionContent: project.name || 'No project name'
+    };
+  });
+
+  const onSubmit = (data) => {
+    const options = {
+      method: isItemSelected ? 'PUT' : 'POST',
+      url: isItemSelected ? `${URL}/tasks/${selectedItem._id}` : `${URL}/tasks`,
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        nameProject: data.nameProject,
+        week: data.week,
+        day: data.day,
+        description: data.descrption,
+        hours: data.hours
+      })
+    };
+    isItemSelected ? dispatch(editTask(options)) : dispatch(postTask(options));
   };
 
   useEffect(() => {
@@ -49,96 +57,73 @@ const Form = () => {
       });
   }, []);
 
-  useEffect(() => {
-    if (isItemSelected) {
-      setProjectValue(selectedItem.nameProjectId || '');
-      setDayValue(selectedItem.day);
-      setWeekValue(selectedItem.week);
-      setHoursValue(selectedItem.hours);
-      setDescriptionValue(selectedItem.description);
-    }
-  }, []);
-
-  const arrayToMapProjects = projects.map((project) => {
-    return {
-      id: project._id,
-      optionContent: project.name || 'No project name'
-    };
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm({
+    mode: 'onChange',
+    resolver: joiResolver(tasksValidation)
   });
-
-  const title = isItemSelected ? 'Update Task' : 'Add Task';
-  const onSubmit = (event) => {
-    event.preventDefault();
-    const options = {
-      method: isItemSelected ? 'PUT' : 'POST',
-      url: isItemSelected ? `${URL}/tasks/${selectedItem._id}` : `${URL}/tasks`,
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        nameProject: projectValue,
-        week: weekValue,
-        day: dayValue,
-        description: descriptionValue,
-        hours: hoursValue
-      })
-    };
-    isItemSelected ? dispatch(editTask(options)) : dispatch(postTask(options));
-  };
 
   return (
     <div className={styles.container}>
       <h2>{title}</h2>
-      <SharedForm onSubmit={onSubmit}>
+      <SharedForm onSubmit={handleSubmit(onSubmit)}>
         <Select
           label="Project"
-          id="project"
-          name="project"
-          value={projectValue}
-          onChange={onChangeProject}
-          required
-          placeholder="Select Project"
+          id="nameProject"
+          name="nameProject"
           arrayToMap={arrayToMapProjects}
-        />
-        <Input
-          label="Week"
-          type="number"
-          id="week"
-          name="week"
-          value={weekValue}
-          onChange={onChangeWeek}
+          placeholder="Select Project"
+          register={register}
+          error={errors.nameProject?.message}
+          value={selectedItem ? selectedItem.nameProject : ''}
           required
-          placeholder="Week"
-        />
-        <Input
-          label="Day"
-          type="number"
-          id="day"
-          name="day"
-          value={dayValue}
-          onChange={onChangeDay}
-          required
-          placeholder="Day"
         />
         <Input
           label="Description"
+          id="description"
+          name="description"
           type="text"
-          id="Description"
-          name="Description"
-          value={descriptionValue}
-          onChange={onChangeDescription}
-          required
           placeholder="Description"
+          register={register}
+          error={errors.description?.message}
+          value={selectedItem ? selectedItem.description : ''}
+          required
+        />
+        <Input
+          label="Week"
+          id="week"
+          name="week"
+          type="text"
+          placeholder="Week"
+          register={register}
+          error={errors.week?.message}
+          value={selectedItem ? selectedItem.week : ''}
+          required
+        />
+        <Input
+          label="Day"
+          id="day"
+          name="day"
+          type="text"
+          placeholder="Day"
+          register={register}
+          error={errors.day?.message}
+          value={selectedItem ? selectedItem.day : ''}
+          required
         />
         <Input
           label="Hours"
-          type="number"
           id="hours"
           name="hours"
-          value={hoursValue}
-          onChange={onChangeHours}
-          required
+          type="text"
           placeholder="Hours"
+          register={register}
+          error={errors.hours?.message}
+          value={selectedItem ? selectedItem.hours : ''}
+          required
         />
       </SharedForm>
       <Modal
