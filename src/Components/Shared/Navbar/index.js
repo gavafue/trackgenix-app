546cef6b997e19a8a414547440b821b0a46f665a
Paula Rinaldi <paurinaldi95@gmail.com>
2022-06-24 14:22:05 -0300

    TG-100: Improved code, used location instead of comparing two arrays to define if employee or default data should go on photo and name

--- src/Components/Shared/Navbar/index.js
+++ src/Components/Shared/Navbar/index.js
@@ -9,7 +9,7 @@ const Navbar = () => {
   const location = useLocation().pathname;
   const navBarItems = location.includes('/employee/') ? employeeNavbar : defaultNavbaritems;
   const employeeName =
-    employeeLogged.firstName && employeeLogged.lastName != undefined
+    employeeLogged.firstName && employeeLogged.lastName
       ? `${employeeLogged.firstName} ${employeeLogged.lastName}`
       : '';
   return (
@@ -17,13 +17,13 @@ const Navbar = () => {
       <div className={styles.appName}>
         <div className={styles.userContainer}>
           <div className={styles.userName}>
-            {navBarItems != employeeNavbar ? 'Pepito' : employeeName}
+            {location.includes('/employee/') ? employeeName : 'Pepito'}
           </div>
           <div className={styles.profileImg}>
-            {navBarItems != employeeNavbar ? (
-              <img src="http://www.4x4.ec/overlandecuador/wp-content/uploads/2017/06/default-user-icon-8.jpg" />
-            ) : (
+            {location.includes('/employee/') ? (
               <img src={employeeLogged?.photo}></img>
+            ) : (
+              <img src="http://www.4x4.ec/overlandecuador/wp-content/uploads/2017/06/default-user-icon-8.jpg" />
             )}
           </div>
         </div>
