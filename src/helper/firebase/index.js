86de6159fd09c8bf2235703c9b59aa12da3818a7
unknown <mtatianabp@gmail.com>
2022-06-24 22:14:57 -0300

    TG-103: Firebase installed and helper created

---
+++ src/helper/firebase/index.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAYPkbsT3fVUaPJa3EPgu_mjMgYNdB2DE",
+  authDomain: "basp-2022-samuel-trackgenix.firebaseapp.com",
+  projectId: "basp-2022-samuel-trackgenix",
+  storageBucket: "basp-2022-samuel-trackgenix.appspot.com",
+  messagingSenderId: "884218763261",
+  appId: "1:884218763261:web:990802ee0a5b8683ee6b94"
+};
+ 
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
