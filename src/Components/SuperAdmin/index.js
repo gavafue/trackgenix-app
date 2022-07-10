332d796789ac47828aba9b21c7a1a34f0c1d34a1
Paula Rinaldi <paurinaldi95@gmail.com>
2022-07-10 16:17:21 -0300

    TG-122: Changed Table to shared component table

--- src/Components/SuperAdmin/index.js
+++ src/Components/SuperAdmin/index.js
@@ -1,13 +1,19 @@
 import styles from 'Components/SuperAdmin/index.module.css';
-import Table from 'Components/SuperAdmin/Table';
+import Table from 'Components/Shared/Table';
 import Modal from 'Components/Shared/Modal';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { getAdmins } from 'redux/admins/thunks';
 import DeleteMessage from 'Components/Shared/DeleteMessage';
 import Preloader from 'Components/Shared/Preloader';
-import { showFeedbackMessage } from 'redux/admins/actions';
+import {
+  showFeedbackMessage,
+  setInfoForDelete,
+  // showDeleteMessage,
+  getSelectedAdmin
+} from 'redux/admins/actions';
 import FeedbackMessage from 'Components/Shared/FeedbackMessage';
+import { useHistory } from 'react-router-dom';
 
 const SuperAdmin = () => {
   const dispatch = useDispatch();
@@ -17,11 +23,20 @@ const SuperAdmin = () => {
   const [showModal, setShowModal] = useState(false);
   const isPending = useSelector((state) => state.admins.isPending);
   const feedbackInfo = useSelector((state) => state.admins.infoForFeedback);
-  // const deleteInfo = useSelector((state) => state.admins.infoForDelete);
+  const deleteInfo = useSelector((state) => state.admins.infoForDelete);
   // const showDelete = useSelector((state) => state.admins.showDeleteMessage);
   const showFeedback = useSelector((state) => state.admins.showFeedbackMessage);
+  const history = useHistory();
+  const editData = (row) => {
+    dispatch(getSelectedAdmin(row));
+    history.push(`/superadmin/form`);
+  };
+
+  const deleteHandler = () => {
+    console.log('holi');
+  };
   const admins = useSelector((state) => state.admins.list)
-    .filter((admin) => admin.active === true)
+    // .filter((admin) => admin.active === true)
     .map((admin) => ({
       name: `${admin.name} ${admin.lastName}`,
       location: admin.city,
@@ -35,6 +50,10 @@ const SuperAdmin = () => {
         headers={['name', 'location', 'status']}
         headersName={['Name', 'Location', 'Status']}
         setShowModal={setShowModal}
+        editData={editData}
+        setInfoForDelete={() => {
+          dispatch(setInfoForDelete());
+        }}
       />
       <Modal
         isOpen={showModal}
@@ -43,7 +62,15 @@ const SuperAdmin = () => {
         }}
       >
         {' '}
-        <DeleteMessage />
+        <DeleteMessage
+          handleClose={() => {
+            setShowModal(false);
+          }}
+          resourceName={'Admin'}
+          infoForDelete={deleteInfo}
+          deleteItem={deleteHandler}
+          setShowModal={setShowModal}
+        />
       </Modal>
       <Modal
         isOpen={showFeedback}
