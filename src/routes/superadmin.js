e1d72662fb1990781c9f731704704d1a6b9297d8
Gabriel Vazquez <gavf.vazquez@gmail.com>
2022-07-15 16:31:55 -0300

    TG-123: unsolved problem into the superadmin routes render

--- src/routes/superadmin.js
+++ src/routes/superadmin.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-//import SuperAdmins from 'Components/SuperAdmins/index';
+import SuperAdmins from 'Components/SuperAdmins/index';
 import AddNewAdmin from 'Components/SuperAdmin/RegisterAdmin/index';
 const Superadmin = () => {
   return (
     <Switch>
       <Route path="/addAdmin" component={AddNewAdmin} />
+      <Route exact path="/" component={SuperAdmins} />
     </Switch>
   );
 };
