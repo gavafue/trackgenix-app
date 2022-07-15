619a3c46a46c3ad89f1ac9a2a010ae558468bb92
Gabriel Vazquez <gavf.vazquez@gmail.com>
2022-07-15 13:00:46 -0300

    TG-126: modified the actual form according to figma

--- src/Components/Shared/Form/index.js
+++ src/Components/Shared/Form/index.js
@@ -3,11 +3,12 @@ import styles from './form.module.css';
 import Button from '../Button';
 import { useHistory } from 'react-router-dom';
 
-const Form = ({ children, onSubmit, name }) => {
+const Form = ({ children, onSubmit, name, header }) => {
   const history = useHistory();
   return (
     <div className={styles.container}>
       <form className={styles.form} onSubmit={onSubmit} name={name}>
+        {header && <div className={styles.header}>{header}</div>}
         <fieldset className={styles.fieldset}>
           {children}
           <Button type="submit" label="Submit" />
