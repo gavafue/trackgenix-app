19fb72c70a16c73a316c21791f32d35b40f34fa1
OctavioRodriguezPaseyro <101280747+OctavioRodriguezPaseyro@users.noreply.github.com>
2022-07-15 20:21:49 -0300

    TG-134: fixed issues with height when table had few inputs

--- src/Components/Shared/Table/index.js
+++ src/Components/Shared/Table/index.js
@@ -4,23 +4,25 @@ import styles from './table.module.css';
 
 const Table = ({ data, headersName, headers, editData, setShowModal, setInfoForDelete }) => {
   return (
-    <table className={styles.container}>
-      <thead className={styles.headers}>
-        <tr>
-          {headersName.map((headerName, index) => {
-            return <th key={index}>{headerName}</th>;
-          })}
-          <th></th>
-        </tr>
-      </thead>
-      <TableContent
-        data={data}
-        headers={headers}
-        editData={editData}
-        setShowModal={setShowModal}
-        setInfoForDelete={setInfoForDelete}
-      />
-    </table>
+    <section className={styles.container}>
+      <table className={styles.table}>
+        <thead className={styles.headers}>
+          <tr>
+            {headersName.map((headerName, index) => {
+              return <th key={index}>{headerName}</th>;
+            })}
+            <th></th>
+          </tr>
+        </thead>
+        <TableContent
+          data={data}
+          headers={headers}
+          editData={editData}
+          setShowModal={setShowModal}
+          setInfoForDelete={setInfoForDelete}
+        />
+      </table>
+    </section>
   );
 };
 
