7cb1c0062dd3160fa8252bfd6f72c680e1b23185
OctavioRodriguezPaseyro <101280747+OctavioRodriguezPaseyro@users.noreply.github.com>
2022-07-15 17:31:26 -0300

    TG-134: styles updated to look like figma

--- src/Components/Shared/Table/TableContent/index.js
+++ src/Components/Shared/Table/TableContent/index.js
@@ -7,18 +7,18 @@ const TableContent = ({ headers, data, editData, setShowModal, setInfoForDelete 
     <tbody>
       {data.map((row) => {
         return (
-          <tr key={row._id} id={row._id}>
+          <tr key={row._id} id={row._id} className={styles.rows}>
             {headers.map((header, index) => {
               return (
-                <td key={index} className={styles.rows}>
+                <td key={index} className={styles.cell}>
                   {row[header]}
                 </td>
               );
             })}
-            <td className={styles.rows}>
+            <td className={styles.cell}>
               <Button onClick={() => editData(row)} label="Edit" />
             </td>
-            <td className={styles.rows}>
+            <td className={styles.cell}>
               <Button
                 label="Delete"
                 theme="secondary"
